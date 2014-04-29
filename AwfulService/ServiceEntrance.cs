eb443a63d453962843db8d7e92133392fa2f1918
felinegrace <felinegrace@163.com>
2014-04-29 14:34:56 +0800

    add windows service

---
+++ AwfulService/ServiceEntrance.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+using Awful.Utility;
+using Awful.Scheduler;
+
+namespace Awful.WindowsService
+{
+    class ServiceEntrance : System.ServiceProcess.ServiceBase
+    {
+        private AwfulScheduler scheduler = null;
+
+        public ServiceEntrance()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.AutoLog = false;
+            this.ServiceName = "Awful Backup Service";
+            Logger.enable();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Logger.info("ServiceEntrance: starting...");
+            Logger.info("ServiceEntrance: working dir is {0}", AppDomain.CurrentDomain.BaseDirectory);
+            scheduler = new AwfulScheduler();
+            scheduler.start();
+            Logger.info("ServiceEntrance: start.");
+        }
+
+        protected override void OnStop()
+        {
+            Logger.info("ServiceEntrance: stopping...");
+            scheduler.stop();
+            Logger.info("ServiceEntrance: stop.");
+        }
+    }
+}
